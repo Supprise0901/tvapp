038a8f2c11b510e5383ff3fd855dd9bfd3f1cc1d
supprise <zzsupprise@proton.me>
2024-03-10 20:40:31 +0800

    update

--- app/src/main/java/com/github/tvbox/osc/bbox/constant/URL.java
+++ app/src/main/java/com/github/tvbox/osc/bbox/constant/URL.java
@@ -2,22 +2,14 @@ package com.github.tvbox.osc.bbox.constant;
 
 public class URL {
     public static String DOMAIN_NAME_PROXY = "https://mirror.ghproxy.com/";
-    public static final String GITHUB_VERSION_PATH = "https://raw.githubusercontent.com/mlabalabala/box/main/version.json";
-    public static final String APK_PATH = "https://raw.githubusercontent.com/mlabalabala/box/main/Release/app-release.apk";
-    // public static final String DOMAIN_NAME_PROXY = "http://192.168.3.159:81";
-    // public static final String GITHUB_VERSION_PATH = "/chfs/shared/Github_Project/box/version.json";
-    // public static final String APK_PATH = "/chfs/shared/Github_Project/box/Release/app-release.apk";
-    // https://raw.staticdn.net/mlabalabala/TVResource/main/boxCfg/ori_source.json
-    // https://raw.gitmirror.com/mlabalabala/TVResource/main/boxCfg/ori_source.json
-    // https://ghproxy.com/https://raw.githubusercontent.com//mlabalabala/TVResource/main/boxCfg/ori_source.json
-    // https://raw.bunnylblbblbl.eu.org/https://raw.githubusercontent.com/mlabalabala/TVResource/main/boxCfg/ori_source.json
+    public static final String GITHUB_VERSION_PATH = "https://raw.githubusercontent.com/Supprise0901/TVbox/main/version.json";
+    public static final String APK_PATH = "https://raw.githubusercontent.com/Supprise0901/TVbox/main/Release/app-release.apk";
     // 默认线路
-    public static final String DEFAULT_API_URL = "https://raw.githubusercontent.com/mlabalabala/TVResource/main/boxCfg/default";
-    // public static final String DEFAULT_API_URL = "https://raw.githubusercontent.com/gaotianliuyun/gao/master/0821.json";
+    public static final String DEFAULT_API_URL = "https://raw.githubusercontent.com/Supprise0901/Fetch/main/tvbox/影视.json";
     // 默认仓库线路
-    public static final String DEFAULT_STORE_API_URL = "https://raw.githubusercontent.com/mlabalabala/TVResource/main/boxCfg/ori_source.json";
+    public static final String DEFAULT_STORE_API_URL = "https://raw.githubusercontent.com/Supprise0901/TVBox_live/main/warehouse.txt";
     // 默认直播线路
-    public static final String DEFAULT_LIVE_URL = "https://raw.githubusercontent.com/mlabalabala/TVResource/main/boxCfg/live.txt";
+    public static final String DEFAULT_LIVE_URL = "https://raw.githubusercontent.com/Supprise0901/TVBox_live/main/live.txt";
 
 
 }
